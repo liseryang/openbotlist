cf295eee580dd6518acfc02a5fae46a0f683634e
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-06 02:12:40 +0000

    more updates to schema fixes

--- trunk/openbotlist/db/create_tables.sql
+++ trunk/openbotlist/db/create_tables.sql
@@ -591,11 +591,28 @@ insert into core_users(user_name, user_password, user_email, date_of_birth, acco
 
 insert into profile_settings(user_id, created_on) values(LAST_INSERT_ID(), NOW());
 
+insert into core_users(user_name, user_password, user_email, date_of_birth, account_number, active_code, last_login_success, last_login_failure, created_on, updated_on) VALUES(
+    'botrover99', 'fa911d71b19af603c0f8a10455a670ef', 'botrover99@email.com', '1981-01-01', '55ebef6779f025f9174478e6abcd3874botrover99', 1, NOW(), NOW(), NOW(), NOW());
+
+insert into core_users(user_name, user_password, user_email, date_of_birth, account_number, active_code, last_login_success, last_login_failure, created_on, updated_on) VALUES(
+    'botbert99', 'fa911d71b19af603c0f8a10455a670ef', 'botbert99@email.com', '1981-01-01', 'b2acafa3c71be70c5aed5aedad365342botbert99', 1, NOW(), NOW(), NOW(), NOW());
+
 --
 -- Insert Default Link Groups
 
 insert into link_groups(group_name, generated_id, created_on) values('Info Articles', 'aaf9dfb546f650d5fa614156000info', NOW());			
 insert into link_groups(group_name, generated_id, created_on) values('Media', 'fd50091908d57ab8b15db358000media', NOW());
 insert into link_groups(group_name, generated_id, created_on) values('NSFW', '65ebdbd0e6a0a67c029000nsfw', NOW());
+
+-- Update the entity_links
+
+--update entity_links a
+-- 	set user_id = (select id from core_users b 
+-- 	where a.full_name = b.user_name);
+
+-- Insert into user links
+
+--insert into user_entity_links(user_id, link_id, created_on) 
+--	select user_id, id, NOW() from entity_links limit 0, 200;
 
 -- End of file
