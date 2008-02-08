a2741360eff8343500ee63fb95ddc58b746e4ff5
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-08 01:10:05 +0000

    adding session request bean to spring config

--- trunk/openbotlist/apps_src/scala/lift/agents/RemoteAgents.scala
+++ trunk/openbotlist/apps_src/scala/lift/agents/RemoteAgents.scala
@@ -7,7 +7,9 @@ package org.spirit.lift.agents
 import java.util.Random
 import org.springframework.context.{ApplicationContext => AC}
 import org.spirit.dao.impl.{BotListUserVisitLogDAOImpl => LogDAO}
+import org.spirit.dao.impl.{BotListSessionRequestLogDAOImpl => SessDAO}
 import org.spirit.bean.impl.{BotListUserVisitLog => Log}
+import org.spirit.bean.impl.{BotListSessionRequestLog => Sess}
 import net.liftweb.http._
 import net.liftweb.http.S._
 import net.liftweb.http.S
@@ -21,6 +23,7 @@ object AgentUtil {
    * Create a unique string id based on; random number and client ip.
    */
   def uniqueMsgId (clientip: String) : String = {
+	val t = (System.currentTimeMillis) + 1
 	val r = new Random()
 	val rand_long = r.nextLong
 	hexEncode(md5( (clientip + rand_long).getBytes ))
@@ -34,6 +37,17 @@ object AgentUtil {
 	link.setRemoteHost(request.getRemoteAddr())
 	link.setUserAgent(request.getHeader("user-agent"))
 	dao.createVisitLog(link)
+  }
+  def buildRequestSession (dao: SessDAO, request: HttpServletRequest, key:String, value:String):String = {
+	val sess = new Sess()
+	val remote_host = request.getRemoteAddr
+	val u_id = uniqueMsgId(remote_host)
+	sess.setRemoteHost(remote_host)
+	sess.setMsgKey(key)
+	sess.setMsgValue(value)
+	sess.setRequestId(u_id)
+	dao.createSessionLog(sess)
+	u_id
   }
 
   def getAC(request: HttpServletRequest) = {
@@ -54,6 +68,9 @@ class RemoteAgents (val request: RequestState, val httpRequest: HttpServletReque
 	// Cast to the user visit log bean (defined in the spring configuration)
 	val log_obj = AgentUtil.getAC(httpRequest).getBean("userVisitLogDaoBean")
 	val log_dao = log_obj.asInstanceOf[LogDAO]
+	val sess_obj = AgentUtil.getAC(httpRequest).getBean("sessionRequestLogDaoBean")
+	val sess_dao = sess_obj.asInstanceOf[SessDAO]
+	val uniq_id = AgentUtil.buildRequestSession(sess_dao, httpRequest, "request_auth", "true")
 	AgentUtil.auditLogPage(log_dao, httpRequest, "remote_agent")
 
     XmlResponse(
@@ -63,7 +80,7 @@ class RemoteAgents (val request: RequestState, val httpRequest: HttpServletReque
  		<botmsg:botid>serverbot</botmsg:botid>
  		<botmsg:message>Hello my name is serverbot, go ahead with your request</botmsg:message>
  		<botmsg:status>200</botmsg:status>
- 		<botmsg:requestid>{ Text(AgentUtil.uniqueMsgId(httpRequest.getRemoteAddr)) }</botmsg:requestid>
+ 		<botmsg:requestid>{ Text(uniq_id) }</botmsg:requestid>
  		<botmsg:majorvers>0</botmsg:majorvers>
  		<botmsg:minorvers>0</botmsg:minorvers>
  	</botmsg:agentmsg>
