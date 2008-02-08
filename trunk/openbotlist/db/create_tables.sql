2b63aa46a2d7320bdcb143980c67a1ddb2aa9ae1
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-08 01:05:31 +0000

    adding session request log table

--- trunk/openbotlist/db/create_tables.sql
+++ trunk/openbotlist/db/create_tables.sql
@@ -518,6 +518,21 @@ CREATE TABLE user_visit_log (
 );
 
 --*********************************************************
+-- Request session log, session log for REST system
+-- Updated: 2/2/2008
+--*********************************************************
+CREATE TABLE session_request_log (
+	request_id  varchar(50) NOT NULL UNIQUE,
+	remote_host varchar(20) NOT NULL,
+	msg_value varchar(40) NOT NULL,
+	msg_key varchar(40) NOT NULL,
+	param_key varchar(40),
+	param_value varchar(40),
+	created_on  DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	PRIMARY KEY (request_id)
+);
+
+--*********************************************************
 -- Insert initial data for botlist
 -- Updated: 2/2/2008
 --*********************************************************
