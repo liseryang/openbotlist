5251ca8a7ad28dc643af84e8182f5375aa51bacc
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-11 06:16:01 +0000

    adding updates to spider remote

--- botlistprojects/botspider/spider/lib/java/spider_remote/scala/SpiderRemote.scala
+++ botlistprojects/botspider/spider/lib/java/spider_remote/scala/SpiderRemote.scala
@@ -6,6 +6,9 @@ package org.spirit.spiderremote
 import scala.xml._
 import java.sql.{DriverManager}
 import org.spirit.loadtest.{LoadTestManager}
+import scala.collection.jcl.{HashMap, Map}
+
+import org.spirit.spiderremote.model.{AgentMessage, TypePayload}
 
 object SpiderRemote {
 
@@ -14,10 +17,11 @@ object SpiderRemote {
     Class.forName("org.sqlite.JDBC")
     val conn = DriverManager.getConnection("jdbc:sqlite:../../../var/lib/spiderdb/contentdb/spider_queue.db")
     val stat = conn.createStatement()
+	Console.println(conn)
 	val rs = stat.executeQuery("select * from remotequeue")
+
 	while (rs.next()) {
 	  Console.println("name = " + rs.getString("url"))
-	  Console.println("title = " + rs.getString("title"))
 	}
 	// Connect to the server
 	LoadTestManager.verifySystemDirs
@@ -26,11 +30,36 @@ object SpiderRemote {
 	val b = serv_agent_doc \\ "message"
 	Console.println(b)
 
+	// Build the type payload list
+	val a = new TypePayload {
+	  val typeName = "linktype"
+	  val title = "The Google"
+	  val url = "http://www.google77.com"
+	  val keywords = "google, google"
+	  val descr = "The descr"
+	}
+	val d = new TypePayload {
+	  val typeName = "linktype2"
+	  val title = "The Google2"
+	  val url = "http://www.google89.com"
+	  val keywords = "google, google4"
+	  val descr = "The descr2"
+	}
 	// Post to server
-	val map = Map.empty[String, String]
-	val v = new java.util.HashMap
-	val res_from_snd = LoadTestManager.postData(v, "http://127.0.0.1:8080/botlist/lift/pipes/types/remote_agent_send", false)
+	val m = new java.util.HashMap
+	val map = new HashMap[String, String](m)
+	val types_msg_payload = new AgentMessage {
+	  val message = "I enjoyed my cake."
+	  val status = 200
+	  val agentName = "botspiderremote"
+	  val messageReqId = "123"
+	  val types = List(a, d)
+	}
+	
+	map("types_payload") = types_msg_payload.toXML.toString
+	val res_from_snd = LoadTestManager.postData(m, "http://127.0.0.1:8080/botlist/lift/pipes/types/remote_agent_send", false)
 	Console.println(res_from_snd(1))
 	Console.println("done");
   }
 }
+
