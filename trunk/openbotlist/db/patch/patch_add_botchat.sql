5960f95779c8573a2c4923d4e682010cc4487463
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-11 02:05:08 +0000

    add botchat forum table creation

---
+++ trunk/openbotlist/db/patch/patch_add_botchat.sql
@@ -0,0 +1,10 @@
+-- 
+-- 9/4/2007
+
+connect botlist_development;
+
+INSERT INTO forum_group(forum_name,
+	forum_descr, keywords, created_on) 
+	VALUES ('Bot Chat', 'Bot Chat', 'bot chat forums', NOW());
+	
+-- End of File
