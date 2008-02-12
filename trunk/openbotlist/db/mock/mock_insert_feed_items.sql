e470df7931f7875744b5c143d43775d0642373b9
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-12 23:24:07 +0000

    additions to feed item mock tests

--- trunk/openbotlist/db/mock/mock_insert_feed_items.sql
+++ trunk/openbotlist/db/mock/mock_insert_feed_items.sql
@@ -5,13 +5,13 @@
 connect botlist_development;
 
 INSERT INTO system_feed_items(main_url, url_title, url_description, url_source, process_count, created_on) 
-		VALUES('http://www.reddit.com', 'The Reddit', 'the reddit', 'http://www.google.com', 0, NOW());
+		VALUES('http://www.reddit.com', 'The Reddit', 'The Reddit is Cool', 'http://www.google.com', 0, NOW());
 
 INSERT INTO system_feed_items(main_url, url_title, url_description, url_source, process_count, created_on) 
-		VALUES('http://www.google.com', 'The Google', 'the google', 'http://www.google.com', 0, NOW());
+		VALUES('http://www.google.com', 'The Google', 'The google is awesome', 'http://www.google.com', 0, NOW());
 		
 INSERT INTO system_feed_items(main_url, url_title, url_description, url_source, process_count, created_on) 
-		VALUES('http://www.yahoo.com', 'The Yahoo', 'the yahoo', 'http://www.yahoo.com', 0, NOW());
+		VALUES('http://www.yahoo.com', 'The Yahoo<b>Yea</b>', 'The yahoo uses tags <b>Testing</b><a href="err">err</a>', 'http://www.yahoo.com', 0, NOW());
 		
 -- End of the script
 
