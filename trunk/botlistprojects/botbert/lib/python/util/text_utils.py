7d901d245443ba6df7601f6be9e9881bec7515c3
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-02-14 06:56:27 +0000

    see the thruth, the path lies within

--- trunk/botlistprojects/botbert/lib/python/util/text_utils.py
+++ trunk/botlistprojects/botbert/lib/python/util/text_utils.py
@@ -2,9 +2,73 @@
  Berlin Brown
  text_utils
  8/20/2007
+
+ Updated: 2/10/2008
+ Added source to format a unicode str to ascii
+
+ References:
+ See beautiful soup for HTML entity conversion.
+ http://www.crummy.com/software/BeautifulSoup/documentation.html
 """
 
 import sys
+from soup.BeautifulSoup import *
+
+def formatStrAscii(val):
+	""" Filter and encode the line in the html documents
+	@see crawlSingleURLForContent
+	"""
+	#TODO: analyze prev code.
+	#return strval.decode('ascii', 'replace').encode('ascii', 'replace') 
+	try:
+		val = val.decode('utf8', 'replace')
+		val = val.encode('ascii', 'replace') 		
+		return val
+	except UnicodeError, e:
+		pass
+	# Always return a value
+	return ""
+
+def formatHtmlEntities(content):
+	""" Parse the document and convert the HTML (todo: XML) entities
+	to the unicode characters"""
+	return BeautifulStoneSoup(content,
+							  convertEntities=BeautifulStoneSoup.HTML_ENTITIES).contents[0]
+
+
+def filterOnlyTextSoup(soup):
+	"""Use beautiful soup to remove all TAG text"""
+	try:
+		v = soup.string
+		if v == None:
+			c = soup.contents
+			resulttext = ''
+			for txt_obj in c:
+				subtext = filterOnlyTextSoup(txt_obj)
+				resulttext += subtext + ' '				
+			return resulttext
+		else:
+			return v.strip()
+		
+	except Exception, e:
+		print e
+		return ""	
+
+def formatDescrWithSoup(content):
+	"""Format the html descriptions with beautiful soup"""
+	soup = BeautifulSoup(content)
+	res = filterOnlyTextSoup(soup)
+	if (res == None) or (len(res) == 0):
+		# On error or other issues, return empty
+		# TODO: should we return content?
+		return ""
+	else:
+		try:
+			res = formatHtmlEntities(res)
+			return res.strip()
+		except Exception, e:
+			print e
+			return ""
 
 def shorten(str, max_len):
     """Shorten a given string to a smaller number of characters"""
