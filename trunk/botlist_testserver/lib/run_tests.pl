5acc8ca1d9921c85b46a74a8bdde273049eb771b
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-03-05 03:11:41 +0000

    adding more to test server scripts

--- trunk/botlist_testserver/lib/run_tests.pl
+++ trunk/botlist_testserver/lib/run_tests.pl
@@ -13,17 +13,11 @@ $integration_tests_home = "$botlist_home/tests/integration";
 
 print "INFO: Creating mock objects -> \n";
 
-# Change to the botlist home and built the project
-print "INFO: building the botlist project, web frontend.";
-chdir $botlist_home || die "Can't change to botlist directory: $!\n";
-system "ant -f build.xml";
-system "ant -f build.xml scala.compile";
-system "ant -f build.xml tomcat.deploy";
-
 # Need to change to that test directory.
 print "Running at $integration_tests_home \n";
 chdir $integration_tests_home || die "Can't change to integration tests directory: $!\n";
-system "ant -f build.xml setup_tests";
+system "ant -f build.xml tests.ruby.rspec";
+system "ant -f build.xml tests.scala.suite";
 print "INFO: Done\n";
 
 ############################################################
