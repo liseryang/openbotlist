2c769c4d2becdf45fcf68cd4a98dcad26ae0a7ea
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-03-06 09:27:29 +0000

    more on test server, add accept client

--- trunk/botlistprojects/laughingman/orbirclib/inc/irc_server.hrl
+++ trunk/botlistprojects/laughingman/orbirclib/inc/irc_server.hrl
@@ -1,5 +1,5 @@
 %% irc_server.hrl
 %%
--record(irc_server_info, {port=6667}).
+-record(irc_server_info, {port=6667, app_handler=undefined}).
 -record(server_state, {serv_sock, client, state, 
 					   connection_timeout, app_handler=undefined}).
