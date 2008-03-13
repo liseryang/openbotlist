ad310de4778abc7ed9b2ac445766e0ac4105edd4
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-03-13 03:38:26 +0000

    adding foaf type

---
+++ trunk/openbotlist/src/org/spirit/dao/impl/BotListEntityTypeFoafDAOImpl.java
@@ -0,0 +1,22 @@
+/**
+ * Berlin Brown
+ * Nov 9, 2006
+ */
+
+package org.spirit.dao.impl;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ * This is class is used by botverse.
+ * 
+ * @author Berlin Brown
+ * 
+ */
+
+public class BotListEntityTypeFoafDAOImpl extends HibernateDaoSupport implements BotListEntityTypeFoafDAO {
+
+}
