c38e4213b2e89e703a64469ea8f7d42481aee75c
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-03-15 06:23:18 +0000

    updates and cleanups for new foaf changes

--- openbotlist/src/org/spirit/form/BotListCityListingForm.java
+++ openbotlist/src/org/spirit/form/BotListCityListingForm.java
@@ -15,34 +15,42 @@ import org.spirit.form.base.BotListBaseForm;
  * @author Berlin Brown
  * 
  */
-
 public class BotListCityListingForm extends BotListBaseForm {
 
-		private String cityName;
-		private Calendar createdOn;
-		
-		/**
-		 * @return the cityName
-		 */
-		public String getCityName() {
-			return cityName;
-		}
-		/**
-		 * @param cityName the cityName to set
-		 */
-		public void setCityName(String cityName) {
-			this.cityName = cityName;
-		}
-		/**
-		 * @return the createdOn
-		 */
-		public Calendar getCreatedOn() {
-			return createdOn;
-		}
-		/**
-		 * @param createdOn the createdOn to set
-		 */
-		public void setCreatedOn(Calendar createdOn) {
-			this.createdOn = createdOn;
-		}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3672928069333737427L;
+
+	private String cityName;
+
+	private Calendar createdOn;
+
+	/**
+	 * @return the cityName
+	 */
+	public String getCityName() {
+		return cityName;
+	}
+
+	/**
+	 * @param cityName the cityName to set
+	 */
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	/**
+	 * @return the createdOn
+	 */
+	public Calendar getCreatedOn() {
+		return createdOn;
+	}
+
+	/**
+	 * @param createdOn the createdOn to set
+	 */
+	public void setCreatedOn(Calendar createdOn) {
+		this.createdOn = createdOn;
+	}
 }
