3f6a95bed0fcc11678c197274f89ef49b4fbbc1d
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-03-15 07:18:30 +0000

    fix to scala fixes due to 2.7.0

--- openbotlist/apps_src/scala/lift/Boot.scala
+++ openbotlist/apps_src/scala/lift/Boot.scala
@@ -1,3 +1,6 @@
+/**
+ * Boot.scala
+ */
 package bootstrap.liftweb
 
 import net.liftweb.http._
@@ -16,15 +19,16 @@ class Boot {
   def boot {
 	LiftServlet.addToPackages("org.spirit.lift.agents")
 	val dispatcher: LiftServlet.DispatchPf = {         
-	  // In our pseudo REST architecture, 'types' is associated
+	  // In our pseudo REST architecture, 'types' is associated with
 	  // the payload dumps and their types.  
 	  // In layman terms, a type is a article link and attributes.
-	  case RequestMatcher(r, ParsePath("lift" :: "pipes" :: "types" :: c :: _, _,_),_, _) => invokeAgents(r, c)
+	  case RequestMatcher(r, ParsePath("lift" :: "pipes" :: "types" :: c :: _, _,_),_, _) => 
+		invokeAgents(r, c)
     }
 	LiftServlet.addDispatchBefore(dispatcher)
   }
-  private def invokeAgents(request: RequestState, methodName: String)(req: HttpServletRequest): Can[ResponseIt] =
-	createInvoker(methodName, new RemoteAgents(request, req)).flatMap(_() match {
+  private def invokeAgents(request: RequestState, methodName: String)(req: RequestState): Can[ResponseIt] =
+	createInvoker(methodName, new RemoteAgents(request)).flatMap(_() match {
 	  case Full(ret: ResponseIt) => Full(ret)
       case _ => Empty
 	})
