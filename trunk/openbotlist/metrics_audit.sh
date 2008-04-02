f91348b429153810fc053bb21f75e6be2c5cbe61
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-02 19:54:28 +0000

    submit metric file cleanup

--- trunk/openbotlist/metrics_audit.sh
+++ trunk/openbotlist/metrics_audit.sh
@@ -3,17 +3,10 @@
 # Get detailed source metrics on the application
 # (run with cygwin)
 
-APP_TYPE="java rb xml jsp"
+APP_TYPE="java rb xml jsp scala"
 
 for i in $APP_TYPE ; do
 	echo "----- APP_TYPE: $i"
-	find . -name \*.$i | xargs wc -l
-done
-
-
-APP_TYPE="py"
-for i in $APP_TYPE ; do
-	echo "**** TEST_TYPE: $i"
 	find . -name \*.$i | xargs wc -l
 done
 
