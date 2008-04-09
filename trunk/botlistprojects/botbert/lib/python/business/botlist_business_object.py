6bf63f20cab18065c61f8efca30393d9b3b11ad2
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-09 06:56:24 +0000

    moving jruby 1.1 to a better location and other code cleanups

--- trunk/botlistprojects/botbert/lib/python/business/botlist_business_object.py
+++ trunk/botlistprojects/botbert/lib/python/business/botlist_business_object.py
@@ -5,31 +5,49 @@
 __author__ = "Berlin Brown"
 __version__ = "0.1"
 
+import sys
 import MySQLdb
+import util.config_util as conf
 
 #--------------------------------------
 class BotlistBusinessObject:
     def __init__(self):
-         self.conn = None
-         self.username = "USER"
-         self.passwd = "PASSWORD"
-         self.db = "botlist_development"
-         self.hostname = "localhost"
-         self.update_count = 0
+        # Load the configuration file with db settings.
+        conf_type = conf.get_parse_config()
+        self.conn = None
+        self.username = conf_type.username
+        self.passwd = conf_type.password
+        self.db = conf_type.database
+        self.hostname = conf_type.hostname
+        self.update_count = 0
          
     def connect(self):
         try:
             self.conn = MySQLdb.connect(host=self.hostname,
-                                   user=self.username, passwd=self.passwd, db=self.db )
+                                        user=self.username, passwd=self.passwd, db=self.db)
+            if self.conn is None:
+                raise Exception("connect(): Could not make connection to database")
+            
             print "INFO: opening connection"
+            return
         except MySQLdb.OperationalError, message:
             errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            sys.exit()
             return
-    
+        except Exception, e:
+            print "ERR: business_object.connect() %s" % e
+            raise Exception("connect(): Could not make connection to database")
+        sys.exit()
+        
     def closeConn(self):
         print "INFO: closing connection"
-        self.conn.close()
-        
+        if self.conn is None:
+            print "ERR: Invalid connection, connection is not open."
+            print "ERR: Check the database connection settings."
+        else:
+            self.conn.close()
+            
     def createCursor(self, obj):
         try:
             pass
--- trunk/botlistprojects/botbert/lib/python/business/entity_link_bom.py
+++ trunk/botlistprojects/botbert/lib/python/business/entity_link_bom.py
@@ -37,147 +37,147 @@ PROCESS_STATUS_CATEGORY = 2
 PROCESS_STATUS_ANALYTICS = 3
 
 class EntityLink:
-	def __init__(self):
-		self.mainUrl = None
-		self.urlTitle = None
-		self.obj_id = None
-		self.keywords = None
-		self.urlDescription = None
+    def __init__(self):
+        self.mainUrl = None
+        self.urlTitle = None
+        self.obj_id = None
+        self.keywords = None
+        self.urlDescription = None
 
 #--------------------------------------
-class EntityLinkHandler(BotlistBusinessObject):		
-	
-	def updateKeywords(self, link, new_keywords):
-		try:
-			cursor = self.conn.cursor()
-			sql_set = []			
-			query = "update entity_links set keywords ='%s', updated_on = NOW(), process_count = 1 where id = %s"
-			sql_set.append(query)
-			sql_str = ''.join(sql_set)
-			cursor.execute(sql_str % (new_keywords, link.obj_id))
-			cursor.close()
-			self.update_count = self.update_count + 1
-		except MySQLdb.OperationalError, message:
-			errorMessage = "ERR %d:\n%s" % (message[0], message[1])
-			print errorMessage
-			return None
-	
-	def updateCategory(self, link, link_category, process_count):
-		""" Update the entity link link_type"""
-		try:
-			cursor = self.conn.cursor()
-			sql_set = []			
-			query = "update entity_links set link_type ='%s', updated_on = NOW(), process_count = %s where id = %s"
-			sql_set.append(query)
-			sql_str = ''.join(sql_set)
-			cursor.execute(sql_str % (link_category, process_count, link.obj_id))
-			cursor.close()
-			self.update_count = self.update_count + 1
-		except MySQLdb.OperationalError, message:
-			errorMessage = "ERR %d:\n%s" % (message[0], message[1])
-			print errorMessage
-			return None
+class EntityLinkHandler(BotlistBusinessObject):     
+    
+    def updateKeywords(self, link, new_keywords):
+        try:
+            cursor = self.conn.cursor()
+            sql_set = []            
+            query = "update entity_links set keywords ='%s', updated_on = NOW(), process_count = 1 where id = %s"
+            sql_set.append(query)
+            sql_str = ''.join(sql_set)
+            cursor.execute(sql_str % (new_keywords, link.obj_id))
+            cursor.close()
+            self.update_count = self.update_count + 1
+        except MySQLdb.OperationalError, message:
+            errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            return None
+    
+    def updateCategory(self, link, link_category, process_count):
+        """ Update the entity link link_type"""
+        try:
+            cursor = self.conn.cursor()
+            sql_set = []            
+            query = "update entity_links set link_type ='%s', updated_on = NOW(), process_count = %s where id = %s"
+            sql_set.append(query)
+            sql_str = ''.join(sql_set)
+            cursor.execute(sql_str % (link_category, process_count, link.obj_id))
+            cursor.close()
+            self.update_count = self.update_count + 1
+        except MySQLdb.OperationalError, message:
+            errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            return None
 
-	def updateWebAnalytics(self, link,
-						   web_model,
-						   process_id_status):
-		""" Update with page analytic data"""
-		try:
-			cursor = self.conn.cursor()
-			sql_set = []
-			q = []
-			#******************************
-			# First, build the SQL string with parameter variables
-			# then set the parameter values 
-			#******************************
-			q.append("update entity_links")
-			q.append(" set links_ct = %s")         # (1)
-			q.append(" ,image_ct = %s")            # (2)
-			q.append(" ,meta_descr_len = %s")      # (3)
-			q.append(" ,meta_keywords_len = %s")   # (4)
-			q.append(" ,meta_descr_wct = %s")      # (5)
-			q.append(" ,meta_keywords_wct = %s")   # (6)
-			q.append(" ,document_size = %s")       # (7)
-			q.append(" ,request_time = %s")        # (8)
-			q.append(" ,para_tag_ct = %s")       # (9)
-			q.append(" ,updated_on = NOW(), process_count = %s where id = %s")
-			query = ''.join(q)
-			sql_set.append(query)
-			sql_str = ''.join(sql_set)
-			cursor.execute(sql_str % (
-				web_model.links_ct,
-				web_model.image_ct,
-				web_model.meta_descr_len,
-				web_model.meta_keywords_len,
-				web_model.meta_descr_wct,
-				web_model.meta_keywords_wct,
-				web_model.document_size,
-				web_model.request_time,
-				web_model.para_tag_ct,
-				process_id_status,
-				link.obj_id))
-			cursor.close()
-			self.update_count = self.update_count + 1
-		except MySQLdb.OperationalError, message:
-			errorMessage = "ERR %d:\n%s" % (message[0], message[1])
-			print errorMessage
-			return None			
+    def updateWebAnalytics(self, link,
+                           web_model,
+                           process_id_status):
+        """ Update with page analytic data"""
+        try:
+            cursor = self.conn.cursor()
+            sql_set = []
+            q = []
+            #******************************
+            # First, build the SQL string with parameter variables
+            # then set the parameter values 
+            #******************************
+            q.append("update entity_links")
+            q.append(" set links_ct = %s")         # (1)
+            q.append(" ,image_ct = %s")            # (2)
+            q.append(" ,meta_descr_len = %s")      # (3)
+            q.append(" ,meta_keywords_len = %s")   # (4)
+            q.append(" ,meta_descr_wct = %s")      # (5)
+            q.append(" ,meta_keywords_wct = %s")   # (6)
+            q.append(" ,document_size = %s")       # (7)
+            q.append(" ,request_time = %s")        # (8)
+            q.append(" ,para_tag_ct = %s")       # (9)
+            q.append(" ,updated_on = NOW(), process_count = %s where id = %s")
+            query = ''.join(q)
+            sql_set.append(query)
+            sql_str = ''.join(sql_set)
+            cursor.execute(sql_str % (
+                web_model.links_ct,
+                web_model.image_ct,
+                web_model.meta_descr_len,
+                web_model.meta_keywords_len,
+                web_model.meta_descr_wct,
+                web_model.meta_keywords_wct,
+                web_model.document_size,
+                web_model.request_time,
+                web_model.para_tag_ct,
+                process_id_status,
+                link.obj_id))
+            cursor.close()
+            self.update_count = self.update_count + 1
+        except MySQLdb.OperationalError, message:
+            errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            return None         
 
-	def updateObjectId(self, link, web_analysis, object_id_status):
-		""" Update the entity link object ID.  Take care in making
-		sure that this object id is not modified (static instance)
-		(see gem_web_analysis for how web analytics is updated).
-		"""
-		try:
-			cursor = self.conn.cursor()
-			sql_set = []			
-			query = "update entity_links set generated_obj_id ='%s', updated_on = NOW(), object_id_status = %s where id = %s"
-			sql_set.append(query)
-			sql_str = ''.join(sql_set)
-			cursor.execute(sql_str % (web_analysis.generated_obj_id,
-									  object_id_status, link.obj_id))
-			cursor.close()
-			self.update_count = self.update_count + 1
-		except MySQLdb.OperationalError, message:
-			errorMessage = "ERR %d:\n%s" % (message[0], message[1])
-			print errorMessage
-			return None
-			
-	def listEntityLinks(self, result_limit=10, where_clause=None):
-		data = None
-		try:
-			cursor = self.conn.cursor()
-			sql_set = []
-			if where_clause:
-				where_str = where_clause
-			else:
-				where_str = ""				
-			query = "select main_url, url_title, id, keywords from entity_links %s order by created_on desc LIMIT 0, %s" % (where_clause, result_limit)
-			sql_set.append(query)
-			sql_str = ''.join(sql_set)
-			cursor.execute(sql_str)
-			data = cursor.fetchall() 
-			self.fields = cursor.description                         
-			cursor.close()
-		except MySQLdb.OperationalError, message:
-			errorMessage = "ERR %d:\n%s" % (message[0], message[1])
-			print errorMessage
-			return None        
-		if not data:
-			raise "ERR: No data returned during listSystemFeedItems() call (try to run a scan on feeds)."        
-		transform_data = []
-		errs_found = 0
-		for row in data:
-			try:
-				link_item = EntityLink()
-				link_item.mainUrl = cgi.escape(row[0])
-				link_item.urlTitle = cgi.escape(row[1])
-				link_item.obj_id = int(row[2])
-				link_item.keywords = cgi.escape(row[3])
-				transform_data.append(link_item)				
-			except Exception, e:
-				errs_found = errs_found + 1
-				
-		return transform_data
+    def updateObjectId(self, link, web_analysis, object_id_status):
+        """ Update the entity link object ID.  Take care in making
+        sure that this object id is not modified (static instance)
+        (see gem_web_analysis for how web analytics is updated).
+        """
+        try:
+            cursor = self.conn.cursor()
+            sql_set = []            
+            query = "update entity_links set generated_obj_id ='%s', updated_on = NOW(), object_id_status = %s where id = %s"
+            sql_set.append(query)
+            sql_str = ''.join(sql_set)
+            cursor.execute(sql_str % (web_analysis.generated_obj_id,
+                                      object_id_status, link.obj_id))
+            cursor.close()
+            self.update_count = self.update_count + 1
+        except MySQLdb.OperationalError, message:
+            errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            return None
+            
+    def listEntityLinks(self, result_limit=10, where_clause=None):
+        data = None
+        try:
+            cursor = self.conn.cursor()
+            sql_set = []
+            if where_clause:
+                where_str = where_clause
+            else:
+                where_str = ""              
+            query = "select main_url, url_title, id, keywords from entity_links %s order by created_on desc LIMIT 0, %s" % (where_clause, result_limit)
+            sql_set.append(query)
+            sql_str = ''.join(sql_set)
+            cursor.execute(sql_str)
+            data = cursor.fetchall() 
+            self.fields = cursor.description                         
+            cursor.close()
+        except MySQLdb.OperationalError, message:
+            errorMessage = "ERR %d:\n%s" % (message[0], message[1])
+            print errorMessage
+            return None        
+        if not data:
+            raise "ERR: No data returned during listSystemFeedItems() call (try to run a scan on feeds)."        
+        transform_data = []
+        errs_found = 0
+        for row in data:
+            try:
+                link_item = EntityLink()
+                link_item.mainUrl = cgi.escape(row[0])
+                link_item.urlTitle = cgi.escape(row[1])
+                link_item.obj_id = int(row[2])
+                link_item.keywords = cgi.escape(row[3])
+                transform_data.append(link_item)                
+            except Exception, e:
+                errs_found = errs_found + 1
+                
+        return transform_data
 
  # End of the file
