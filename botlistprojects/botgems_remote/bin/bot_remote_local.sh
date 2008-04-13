3efb09b8f0553233097c07ccb9d328b0b359ab91
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-13 20:26:35 +0000

    more fixes to botlist gems remoting

--- botlistprojects/botgems_remote/bin/bot_remote_local.sh
+++ botlistprojects/botgems_remote/bin/bot_remote_local.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 # bot remote sync.sh
@@ -26,21 +27,24 @@ fi
 JAVA=$JAVA_HOME/bin/java
 
 BOTBERT_HOME=$ABS_CONF
+# The botlist projects directory contains the system library (jruby)
+PROJECTS_HOME=`dirname $BOTBERT_HOME`
 TOPDIR=$BOTBERT_HOME
 DIR_PROPERTIES=$BOTBERT_HOME
 
-LIB1=$BOTBERT_HOME/lib/botlistloadtest.jar
-LIB2=$BOTBERT_HOME/lib/jruby.jar
-LIB3=$BOTBERT_HOME/lib/ibatis-2.3.0.677.jar
-LIB4=$BOTBERT_HOME/lib/botlistbeans.jar
-LIB5=$BOTBERT_HOME/lib/mysql-connector-java-5.0.3-bin.jar
+LIB1=$PROJECTS_HOME/botprojects_lib/java/lib/jruby.jar
+LIB2=$PROJECTS_HOME/botprojects_lib/java/lib/sqlitejdbc-v043-nested.jar
+LIB3=$BOTBERT_HOME/lib/botlistloadtest.jar
+LIB4=$BOTBERT_HOME/lib/ibatis-2.3.0.677.jar
+LIB5=$BOTBERT_HOME/lib/botlistbeans.jar
+LIB6=$BOTBERT_HOME/lib/mysql-connector-java-5.0.3-bin.jar
 
 if [ $(uname -s | grep -c CYGWIN) -gt 0 ]; then
 	echo "WARN: running in CYGWIN environment"
 	DIR_PROPERTIES=`cygpath -wp $BOTBERT_HOME`
 fi
 
-LIN_CPBOTBERT=".:$LIB1:$LIB2:$LIB3:$LIB4:$LIB5"
+LIN_CPBOTBERT=".:$LIB1:$LIB2:$LIB3:$LIB4:$LIB5:$LIB6"
 
 cd $BOTBERT_HOME
 
--- botlistprojects/botgems_remote/bin/remote_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while true ; do
-	./simpl_remote_prod.sh
-	echo "Done"
-	sleep 30
-done
