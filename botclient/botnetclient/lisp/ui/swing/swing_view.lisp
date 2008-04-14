7d18340b196f38a3f9a880dc1ea1f4156aae8404
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-14 23:30:43 +0000

    adding abcl lisp fixes

--- botclient/botnetclient/lisp/ui/swing/swing_view.lisp
+++ botclient/botnetclient/lisp/ui/swing/swing_view.lisp
@@ -1,9 +1,10 @@
 ;;
-;; swing_view.lisp
+;; ABCL oriented swing_view.lisp
 ;; Author: Berlin Brown
 ;; Date: 4/11/2008
 ;;--------------------------------------
 ;; Swing Client View (abcl oriented swing frame creation)
+;;
 ;; "The opposite of a correct statement is a false statement." -- Niels Bohr
 ;;--------------------------------------
 
@@ -32,82 +33,66 @@
 (defconstant j-scrollpaneconstants "javax.swing.ScrollPaneConstants")
 (defconstant j-uimanager "javax.swing.UIManager")
 
+(defparameter *scroll-h-always* (jfield-raw j-scrollpaneconstants 
+											HORIZONTAL_SCROLLBAR_ALWAY))
+(defparameter *scroll-v-always* (jfield-raw j-scrollpaneconstants
+											VERTICAL_SCROLLBAR_ALWAY))
+
+(defparameter *bl-north* (jfield-raw j-borderlayout "NORTH")
+  "Definition for swing constants")
+(defparameter *bl-east* (jfield-raw j-borderlayout "EAST")
+  "Definition for swing constants")
+(defparameter *bl-center* (jfield-raw j-borderlayout "CENTER")
+  "Definition for swing constants")
+(defparameter *bl-west* (jfield-raw j-borderlayout "WEST")
+  "Definition for swing constants")
+(defparameter *bl-south* (jfield-raw j-borderlayout "SOUTH")
+  "Definition for swing constants")
+
+(defparameter *box-y-axis* (jfield-raw j-boxlayout "Y_AXIS")
+  "Definition for swing constants")
+(defparameter *box-x-axis* (jfield-raw j-boxlayout "X_AXIS")
+  "Definition for swing constants")
+
 ;; ** ABCL Lisp Helpers
 ;;----------------------------
 ;; Example static method call "System.getenv('KEY')
 ;; (jstatic "getenv" (jclass "java.lang.System") "KEY")
 ;;
-;; jfli:new-class:
-;; ---------------------------
-;;
-;; defmacro new-class (
-;;  class-name, super-and-interface-names, 
-;;        constructor-defs, method-defs field-defs)
-;;
-;; Creates, registers and returns a Java object 
-;; that implements the supplied interfaces
-;; --------------------
-;; Arguments: 
-;;
-;;  * [!] class-name -> string
-;;  * [!] super-and-interface-names -> class-name | (class-name interface-name*)
-;;  * [!] constructor-defs -> (constructor-def*)
-;;  * constructor-def -> (ctr-arg-defs body) 
-;;          /the first form in body may be (super arg-name+); this will call the constructor of the superclass
-;;           with the listed arguments/
-;;  * ctr-arg-def -> (arg-name arg-type)
-;;  * method-def -> (method-name return-type access-modifiers arg-defs* body)
-;;          /access-modifiers may be nil (to get the modifiers from the superclass), a keyword, or
-;;           a list of keywords/
-;;  * method-name -> string 
-;;  * arg-def -> arg-name | (arg-name arg-type)
-;;  * arg-type -> \"package.qualified.ClassName\" | classname. | :primitive
-;;  * class-name -> \"package.qualified.ClassName\" | classname. 
-;;  * interface-name -> \"package.qualified.InterfaceName\" | interfacename. 
-;;
-;; Example new-class usage:
-;; (new-class "SERVLET1.Servlet1" "javax.servlet.GenericServlet" ()
-;;	   (("service" :void :public 
-;;		 ((req "javax.servlet.ServletRequest")
-;;		  (resp "javax.servlet.ServletResponse"))
-;;		 (with-simple-restart (abort "Exit to hyperspace")
-;;							  (servlet1-service req resp))))
-;;	   ())
+;; Fields:
+;; (jfield-raw "java.lang.Boolean" "TRUE")
 ;;----------------------------
 
-;; ** Constructors **
+;; ** Swing Constructors **
 ;;----------------------------
 ;; JTextField() / JTextField(String text, int columns)
 ;; JPanel() 
 
 (defconstant *DEFAULT_PATH* "file://./latin_text.txt")
 
-(defun init-swing ()
+(defun init-swing (jframe)
   (progn (setNativeLookUI)
 		 (initTextAreaLayout)
-		 (initMenus) (initActions)))
+		 (initMenus) 
+		 (initActions)))
 
-(defun initTextAreaLayout ()
-  (let ((pathField (jnew 
-					(jconstructor j-jtextfield j-string :int) DEFAULT_PATH 40))
-		(topPanel (jnew 
-				   (jconstructor j-jpanel))))
-	(jcall (jmethod topPanel "setLayout(new BoxLayout" 
-					topPanel BoxLayout.Y_AXIS))
-	(jcall (jmethod topPanel "add" pathField))
-	(let ((JPanel buttonPanel 
-				  (jnew (jconstructor "JPanel")))))
-	(jcall (jmethod this "readRequestsButton@NEW_EXPR_JButton" "Run Action"))
-	(jcall (jmethod buttonPanel "add" this.readRequestsButton))
-	(jcall (jmethod topPanel "add" buttonPanel))
-	(let ((contentArea 
-		   (jnew (jconstructor j-jtextarea :int :int) 25 60))))
-	;;JScrollPane scrollPane@NEW_EXPR_JScrollPane(contentArea 
-	;;											ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-	;;											ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS)
-	(jcall (jmethod getContentPane() "setLayout(new BorderLayout" )))
-  (jcall (jmethod getContentPane() "add" topPanel BorderLayout.NORTH))
-  (jcall (jmethod getContentPane() "add" scrollPane BorderLayout.CENTER)))
+(defun initTextAreaLayout (content-pane)
+  (let* ((pathField (jnew
+					 (jconstructor j-jtextfield j-string "int") DEFAULT_PATH 40))
+		 (topPanel (jnew (jconstructor j-jpanel))))
+	(jcall (jmethod j-jpanel "setLayout") topPanel *box-y-axis*)
+	(jcall (jmethod j-jpanel "add") pathField)
+	(let* ((buttonPanel
+			(jnew (jconstructor "JPanel"))))
+	  (jcall (jmethod j-jpanel "add") topPanel buttonPanel))
+	  (let* ((contentArea
+			  (jnew (jconstructor j-jtextarea "integer" "integer") 25 60))
+			 (scrollPane 
+			  (jnew (jconstructor j-jscrollpane "integer" "integer") 
+					*scroll-v-always* *scroll-h-always*)))
+		(jcall (jmethod "setLayout(new BorderLayout"))
+		(jcall (jmethod getContentPane "add" topPanel *bl-north*))
+		(jcall (jmethod getContentPane "add" scrollPane *bl-center*)))))
 
 (defun initMenus ()
   (let ((JMenuBar bar (jnew (jconstructor j-jmenubar )))
@@ -126,29 +111,16 @@
   (let ((LoadFileAction loadFileAction (jnew (jconstructor "LoadFileAction" <ARG TYPES>) ))))
   (jcall (jmethod readRequestsButton "addActionListener" loadFileAction)))
 
-(defun quit-action ()
-  (jcall (jmethod "java.lang.Runnable" "run")
-		 (jinterface-implementation (jclass "java.lang.Runnable") "run"
-									(lambda () (loop (print "Testing"))))))
-
-;;
-;; Create the new class - extending the abstraction action class.
-(jfli:new-class "Swing.QuitAction" j-abstractaction ()		   
-  (("actionPerformed" :void :public
-;((evt j-actionevent)))) ())
-
 (defun setNativeLookUI ()
-  (jcall (jmethod UIManager 
-				  "setLookAndFeel" 
-				  "com.sun.java.swing.plaf.windows.WindowsLookAndFeel")))
-
+  (jstatic "setLookAndFeel"
+		   (jclass j-uimanager)
+				   "com.sun.java.swing.plaf.windows.WindowsLookAndFeel"))
 (defun createReaderFrame ()
   " Create the simple reader frame "
-  (let ((simpleFrame 
-		 (jnew (jconstructor "SimpleSwing"))))
-	(jcall (jmethod simpleFrame "pack" ))
-	(jcall (jmethod simpleFrame "setVisible" 
-					(make-immediate-object t :boolean)))))
+  (let ((simpleFrame (jnew (jconstructor j-jframe))))
+	(jcall (jmethod j-jframe "pack" ) simpleFrame)
+	(jcall (jmethod j-jframe "setVisible" "boolean") simpleFrame
+		   (make-immediate-object t :boolean))))
 
 (defun lisp-main ()
   (createReaderFrame))
