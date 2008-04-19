228c14c5ca30c6f9f47fa4317534108909be9ce4
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-19 04:41:37 +0000

    clean up on remote utilities

--- trunk/botlistprojects/botgems_remote/bin/bot_remote_local.sh
+++ trunk/botlistprojects/botgems_remote/bin/bot_remote_local.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
 # bot remote sync.sh
@@ -19,7 +18,6 @@ case $0 in
 		ABS_CONF=`dirname $ABS_APP_PATH`
 		;; 
 esac
-
 if [ -z "$JAVA_HOME"  ] ; then
  JAVA_HOME=/usr
 fi
@@ -30,25 +28,14 @@ BOTBERT_HOME=$ABS_CONF
 # The botlist projects directory contains the system library (jruby)
 PROJECTS_HOME=`dirname $BOTBERT_HOME`
 TOPDIR=$BOTBERT_HOME
-DIR_PROPERTIES=$BOTBERT_HOME
 
-LIB1=$PROJECTS_HOME/botprojects_lib/java/lib/jruby.jar
-LIB2=$PROJECTS_HOME/botprojects_lib/java/lib/sqlitejdbc-v043-nested.jar
-LIB3=$BOTBERT_HOME/lib/botlistloadtest.jar
-LIB4=$BOTBERT_HOME/lib/ibatis-2.3.0.677.jar
-LIB5=$BOTBERT_HOME/lib/botlistbeans.jar
-LIB6=$BOTBERT_HOME/lib/mysql-connector-java-5.0.3-bin.jar
+# Use the top project directory to include the correct CP config functions
+. ${TOPDIR}/bin/conf_remote_funcs.sh
 
-if [ $(uname -s | grep -c CYGWIN) -gt 0 ]; then
-	echo "WARN: running in CYGWIN environment"
-	DIR_PROPERTIES=`cygpath -wp $BOTBERT_HOME`
-fi
-
-LIN_CPBOTBERT=".:$LIB1:$LIB2:$LIB3:$LIB4:$LIB5:$LIB6"
 
 cd $BOTBERT_HOME
 
-APP_MAIN=org.jruby.Main
+echo "-----------------------"
 echo "running in directory=${BOTBERT_HOME}"
 echo $LIN_CPBOTBERT
 echo "-----------------------"
