05fc49c68b57215b05e7bb1f4385734d253cf634
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-21 21:24:35 +0000

    adding django/appengine models and forms, skeleton code

---
+++ trunk/botlist_trinity/projects/ghostnet/ghost_forms/core_forms.py
@@ -0,0 +1,130 @@
+
+entity_links
+
+active_media_feeds
+
+media_feeds  
+
+admin_main_banner
+
+search_query_filters
+
+cat_group_terms
+  
+
+cat_link_groups
+
+child_list_links
+
+user_entity_links
+
+
+----------------------
+
+--------------- core_ghostnet_models
+
+CREATE TABLE entity_links (
+
+CREATE TABLE active_media_feeds (
+
+
+CREATE TABLE media_feeds (
+  
+
+CREATE TABLE admin_main_banner (
+
+CREATE TABLE search_query_filters (
+
+CREATE TABLE cat_group_terms (
+  
+
+CREATE TABLE cat_link_groups (
+
+CREATE TABLE child_list_links (
+
+user_entity_links
+
+----------------------
+
+
+--------------- listing_models
+
+
+CREATE TABLE city_listing (
+
+
+CREATE TABLE post_sections (
+
+
+CREATE TABLE post_listing (
+
+
+CREATE TABLE post_image_metadata(
+
+---------------------------
+
+------------------------------ semantic_models
+
+CREATE TABLE entity_type_foaf (
+
+---------------------------
+
+
+--------------------------- group models
+
+CREATE TABLE forum_group (
+
+CREATE TABLE user_comments(
+
+
+CREATE TABLE link_groups (
+
+CREATE TABLE group_links (
+
+--------------------------- user_models
+
+CREATE TABLE core_users (
+
+CREATE TABLE acl_control (
+
+CREATE TABLE group_control (
+
+CREATE TABLE acl_manager (  
+
+CREATE TABLE group_manager (
+
+
+CREATE TABLE profile_settings (
+
+
+CREATE TABLE developer_users (
+  
+
+CREATE TABLE users (
+
+CREATE TABLE authorities (
+
+
+---------------------------- Doc File 
+
+CREATE TABLE doc_file (
+  
+
+CREATE TABLE doc_file_metadata (
+
+
+---------------------------- System files
+
+CREATE TABLE user_visit_log (
+
+CREATE TABLE system_audit_log (
+
+CREATE TABLE system_feed_items (
+CREATE TABLE system_scan_feeds (
+
+CREATE TABLE system_web_files (
+
+user_visit_log
+
+session_request_log
+
