37862e5bfe9ccb67121af369fd81a9146d2efc7f
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-28 02:40:51 +0000

    major changes and fixes to django/appengine botlist port. Added rpc processing, ability to submit data to form, etc.

---
+++ botlist_trinity/projects/ghostnet/ghost_views/ghostnet/rpc_views.py
@@ -0,0 +1,14 @@
+'''
+ Berlin Brown
+ ghost_views.ghostnet.link_views.index
+'''
+
+import datetime
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from google.appengine.api import users
+
+def remote_agent_send():
+	
+# End of Script
