dec1a435d98ad08676719b4a489a3f1fc3b253b6
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-28 02:37:49 +0000

    major changes and fixes to django/appengine botlist port. Added rpc processing, ability to submit data to form, etc.

---
+++ botlist_trinity/projects/ghostnet/business/rpc/remote_agents.py
@@ -0,0 +1,15 @@
+'''
+ Berlin Brown
+'''
+
+__author__ = "Berlin Brown"
+__version__ = "0.1"
+
+from xml.dom.minidom import parse, parseString
+
+def remote_agent_send(data):
+	doc = parseString(data)
+	form = EntityLinksForm(request.POST)
+	return None
+	
+# End of Script
