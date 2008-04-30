b11e87d9e8e4ed982143bcee63427e33919efc7b
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-30 04:22:53 +0000

    major changes and fixes to django/appengine botlist port. Added rpc processing, ability to submit data to form, etc.

--- trunk/botlist_trinity/projects/ghostnet/business/rpc/remote_agents.py
+++ trunk/botlist_trinity/projects/ghostnet/business/rpc/remote_agents.py
@@ -1,5 +1,45 @@
 '''
  Berlin Brown
+
+ Example Server Request Setup - Initial Response (remote_agent_req):
+ -----------------------------
+<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" >
+	<agentmsg>
+ 		<botid>serverbot</botid>
+ 		<message>Hello my name is serverbot.  Would you like some cake?</message>
+ 		<status>200</status>
+ 		<requestid>{ Text(uniq_id) }</requestid>
+ 		<majorvers>0</majorvers>
+ 		<minorvers>0</minorvers>
+ 	</agentmsg>
+</rdf:RDF>
+
+ -----------------------------
+ (*) Example Client Request (remote_agent_send):
+ post key = types_payload
+ -----------------------------
+ <?xml version="1.0" encoding="UTF-8" ?>
+ <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	   <agentmsg>
+		 <botid>botbert</botid>
+ 		 <message>We processed some cake</message>
+ 		 <status>200</status>
+		 <typespayload>
+		   <type>
+		     <title>The Title</title>
+			 <url>http://www.google1.com</url>
+			 <keywords>go google</keywords>
+			 <descr>the google dot com</descr>
+		   </type>
+		 </typespayload>
+	   </agentmsg>
+ </rdf:RDF>
+
+ Example Server Response to Request (confirmation):
+ -----------------------------
+ <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" >
+	  <message>Enjoy your cake</message>
+</rdf:RDF>
 '''
 
 __author__ = "Berlin Brown"
