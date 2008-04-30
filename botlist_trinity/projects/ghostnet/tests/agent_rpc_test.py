0d18f11518b76027000c7832afcf6d985495afa6
berlin.brown <berlin.brown@b3f15b47-0e3a-0410-a62e-5f470dcc47f2>
2008-04-30 04:22:53 +0000

    major changes and fixes to django/appengine botlist port. Added rpc processing, ability to submit data to form, etc.

--- botlist_trinity/projects/ghostnet/tests/agent_rpc_test.py
+++ botlist_trinity/projects/ghostnet/tests/agent_rpc_test.py
@@ -28,7 +28,43 @@ class AgentRpcTest(unittest.TestCase):
 		except Exception, e:
 			print e
 		assert (doc is None)
-		
+
+	def test_parse_example_payload(self):
+		'''Test parsing an example payload message'''
+		types_payload = '''<?xml version="1.0" encoding="UTF-8" ?>
+ <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+	   <agentmsg>
+		 <botid>botbert</botid>
+ 		 <message>We processed some cake</message>
+ 		 <status>200</status>
+		 <typespayload>
+		   <type>
+		     <title>The Title</title>
+			 <url>http://www.google1.com</url>
+			 <keywords>go google</keywords>
+			 <descr>the google dot com</descr>
+		   </type>
+		   <type>
+		     <title>The Title 2</title>
+			 <url>http://www.google2.com</url>
+			 <keywords>go google two</keywords>
+			 <descr>the google dot com two</descr>
+		   </type>
+		 </typespayload>
+	   </agentmsg>
+ </rdf:RDF>'''
+		payload_doc = parseString(types_payload)
+		assert (payload_doc is not None)
+
+		# Get the typespayload data
+		payload_node = payload_doc.getElementsByTagName("typespayload")[0]
+		assert (payload_node is not None)
+
+		types = payload_doc.getElementsByTagName("type")
+		self.assertEquals(2, len(types))
+		url1 = types[0].getElementsByTagName("url")[0].firstChild.nodeValue
+		print url1
+	
 	def test_simple_xml_parse(self):
 		good_data = "<?xml version='1.0' encoding='UTF-8' ?><data></data>"
 		bad_data = "<?xml version='1.0' encoding='UTF-8' ?><data></d"
